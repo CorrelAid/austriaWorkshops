4c5638c89f58775f811355d310777b6cf1d1c6e6
ArneLanglet <arne.langlet@gmail.com>
2023-05-23 19:59:27 +0200

    first query to gfw api for spatial and eez data

--- materials/2023-05-23_GFW_visualisation_development/packages_and_prep.R
+++ materials/2023-05-23_GFW_visualisation_development/packages_and_prep.R
@@ -40,3 +40,69 @@ key <- Sys.getenv("GFW_TOKEN")
 #           key = key, 
 #           start_date = "2022-01-01",
 #           end_date = "2022-01-02")
+
+
+
+# get data for a region
+
+code_eez <- get_region_id(region_name = 'ECU', region_source = 'eez', key = key)
+
+eez_fish_df <- get_raster(spatial_resolution = 'low',
+                          temporal_resolution = 'yearly',
+                          group_by = 'flag',
+                          date_range = '2022-01-01,2022-06-01',
+                          region = code_eez$id[2],
+                          region_source = 'eez')
+
+
+
+
+# get data for network who fished where
+
+## query gfw api 
+usa_longline <- get_vessel_info(
+  query = "flag = 'USA' AND geartype = 'set_longlines'", 
+  search_type = "advanced", 
+  dataset = "fishing_vessel",
+  key = key
+)
+
+
+## paste all vessel ids together
+usa_longline_ids <- paste0(usa_longline$id[1:length(usa_longline)], collapse = ',')
+
+
+## get fishing activity
+df_usa_longline_fishing <- get_event(event_type='fishing',
+          vessel = usa_longline_ids,
+          start_date = "2022-01-01",
+          end_date = "2022-06-01",
+          key = key
+)
+
+
+## get id of eezs in which these activities took place
+
+fished_eezs <- c()
+
+for(i in 1:17){
+   id <- paste0(df_usa_longline_fishing$regions[[i]]$eez[1])
+   fished_eezs <- data.frame(rbind(fished_eezs, id))}
+
+## organize dataframe
+fished_eezs$row <- row.names(fished_eezs)  
+rownames(fished_eezs)<-NULL
+
+# fished_eezs$rbind.fished_eezs..id. %>% as.numeric()
+# 
+# get_region_id(region_name = 8456,
+#               key = key) 
+
+
+## merge eez id with the name of the state  
+fished_eezs %>% select(rbind.fished_eezs..id.) %>% 
+  dplyr::rowwise() %>%
+  dplyr::mutate(eez_name = get_region_id(region_name = as.numeric(rbind.fished_eezs..id.),
+                                         region_source = 'eez',
+                                         key = key)$label)
+
